85a55d0ab2366057222cfb06d904834536150134
Bilal <153359274+Babaevskiyy@users.noreply.github.com>
2024-06-13 04:20:00 +0300

    м

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 require('dotenv').config();
 const helmet = require('helmet');
+const Product = require('./models/products'); // Предполагается, что у вас есть модель Product
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to the database!'))
-  .catch(error => console.error('Database connection error:', error));
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to the database!');
+}).catch(error => {
+    console.error('Database connection error:', error);
+});
 
 app.use(helmet.contentSecurityPolicy({
     directives: {
@@ -24,24 +30,38 @@ app.use(helmet.contentSecurityPolicy({
 }));
 
 // middlewares
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.render("index", { title: "Главная страница" });
-  });
-
 app.use(session({
-  secret: 'my secret key',
-  saveUninitialized: true,
-  resave: false,
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false,
 }));
 
 app.use((req, res, next) => {
-  res.locals.message = req.session.message;
-  delete req.session.message;
-  next();
+    res.locals.message = req.session.message;
+    delete req.session.message; // Удаление сообщения из сессии после передачи
+    next();
 });
+
+app.get("/", async (req, res) => {
+    try {
+        const products = await Product.find(); // Получение продуктов из базы данных
+        res.render("index", {
+            title: "Главная страница",
+            products,
+            user: req.session.user, // Передача пользователя из сессии, если он авторизован
+            message: req.session.message // Передача сообщения из сессии
+        });
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 app.use(express.static("uploads"));
 
@@ -51,5 +71,5 @@ app.set('view engine', 'ejs');
 app.use("", require('./routes/routes'));
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+    console.log(`Server started at http://localhost:${PORT}`);
 });
