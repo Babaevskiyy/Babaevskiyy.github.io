680074dcc84a4a0ea00b45a3acd8cf7a744d2aaa
Bilal <153359274+Babaevskiyy@users.noreply.github.com>
2024-06-13 03:39:30 +0300

    b

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 require('dotenv').config();
+const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,6 +10,18 @@ const PORT = process.env.PORT || 4000;
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to the database!'))
   .catch(error => console.error('Database connection error:', error));
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        styleSrc: ["'self'", "'unsafe-inline'", "stackpath.bootstrapcdn.com"],
+        fontSrc: ["'self'", "stackpath.bootstrapcdn.com"],
+        imgSrc: ["'self'", "data:", "https://avtosklad-4qqua2ge9-babaevskiys-projects.vercel.app"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+    },
+}));
 
 // middlewares
 app.use(express.urlencoded({ extended: false }));
